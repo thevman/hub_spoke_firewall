c0087097f89adbdf2904ee1f23fc0f05f2df5ec1
Vineet Sharma <vsharma@centrilogic.com>
2024-05-29 23:29:01 -0400

    uskng subnet ips for firewall rules

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
     rule {
       destination_fqdn_tags = ["AzureKubernetesService"]
       name                  = "VM-runner-api-fqdn"
-      source_addresses      = ["*"]
+      source_addresses      = var.spoke1_subnet_address_prefixes
       protocols {
         port = 443
         type = "Https"
@@ -79,43 +79,6 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
         type = "Http"
       }
     }
-
-    # rule {
-    #   destination_fqdns = ["*github.com", "*githubusercontent.com"]
-    #   name              = "Allow-github"
-    #   source_addresses  = var.spoke1_address_space
-    #   protocols {
-    #     port = 443
-    #     type = "Https"
-    #   }
-    # }
-    # rule {
-    #   destination_fqdns = ["*.docker.com"]
-    #   name              = "Allow-Docker"
-    #   source_addresses  = var.spoke1_address_space
-    #   protocols {
-    #     port = 443
-    #     type = "Https"
-    #   }
-    # }
-    # rule {
-    #   destination_fqdns = ["apt.releases.hashicorp.com", "releases.hashicorp.com", "registry.terraform.io", "pypi.org", "files.pythonhosted.org", "api0.prismacloud.io", "www.bridgecrew.cloud"]
-    #   name              = "VM-runner-terraformdeployment"
-    #   source_addresses  = var.spoke1_address_space
-    #   protocols {
-    #     port = 443
-    #     type = "Https"
-    #   }
-    # }
-    # rule {
-    #   destination_fqdns = ["mcr.microsoft.com", "*.data.mcr.microsoft.com", "ghcr.io", "management.azure.com", "*.blob.core.windows.net", "k8s.gcr.io", "storage.googleapis.com", "registry.k8s.io", "*.docker.io", "kubernetes.github.io", "login.microsoftonline.com", "us-east4-docker.pkg.dev", "*.azureedge.net", "vault.azure.net", "acs-mirror.azureedge.net"]
-    #   name              = "k8s-runner-cc-01-Https"
-    #   source_addresses  = var.spoke1_address_space
-    #   protocols {
-    #     port = 443
-    #     type = "Https"
-    #   }
-    # }
   }
 
   network_rule_collection {
@@ -127,78 +90,42 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
       destination_ports     = ["1194"]
       name                  = "VM-runner-apiudp"
       protocols             = ["UDP"]
-      source_addresses      = ["*"]
+      source_addresses      = var.spoke1_subnet_address_prefixes
     }
     rule {
       destination_addresses = ["AzureCloud.canadacentral"]
       destination_ports     = ["9000"]
       name                  = "VM-runner-apitcp"
       protocols             = ["TCP"]
-      source_addresses      = ["*"]
+      source_addresses      = var.spoke1_subnet_address_prefixes
     }
     rule {
       destination_fqdns = ["ntp.ubuntu.com"]
       destination_ports = ["123"]
       name              = "VM-runner-time"
       protocols         = ["UDP"]
-      source_addresses  = ["*"]
+      source_addresses  = var.spoke1_subnet_address_prefixes
     }
     rule {
       destination_fqdns = ["ghcr.io", "pkg-containers.githubusercontent.com"]
       destination_ports = ["443"]
       name              = "VM-runner-ghcr"
       protocols         = ["TCP"]
-      source_addresses  = ["*"]
+      source_addresses  = var.spoke1_subnet_address_prefixes
     }
     rule {
       destination_fqdns = ["docker.io", "registry-1.docker.io", "production.cloudflare.docker.com"]
       destination_ports = ["443"]
       name              = "VM-runner-docker"
       protocols         = ["TCP"]
-      source_addresses  = ["*"]
+      source_addresses  = var.spoke1_subnet_address_prefixes
     }
     rule {
       destination_addresses = ["*"]
       destination_ports     = ["53"]
       name                  = "VM-runner-dns"
       protocols             = ["TCP", "UDP"]
-      source_addresses      = ["*"]
+      source_addresses      = var.spoke1_subnet_address_prefixes
     }
-    # rule {
-    #   destination_fqdns = ["apt.releases.hashicorp.com", "releases.hashicorp.com", "github.com", "registry.terraform.io", "pypi.org"]
-    #   destination_ports = ["22"]
-    #   name              = "VM-runner-terraformdeployment01"
-    #   protocols         = ["TCP"]
-    #   source_addresses  = var.spoke1_subnet_address_prefixes
-    # }
-    # rule {
-    #   destination_fqdns = ["production.cloudflare.docker.com"]
-    #   destination_ports = ["*"]
-    #   name              = "k8s-runner-cc-01-docker"
-    #   protocols         = ["Any"]
-    #   source_addresses  = var.spoke1_subnet_address_prefixes
-    # }
-    # rule {
-    #   destination_addresses = ["VirtualNetwork"]
-    #   destination_ports     = ["*"]
-    #   name                  = "k8s-runner-cc-01-Any"
-    #   protocols             = ["Any"]
-    #   source_addresses      = ["*"]
-    # }
   }
-
-  # nat_rule_collection {
-  #   name     = "nat_rule_collection1"
-  #   priority = 300
-  #   action   = "Dnat"
-  #   rule {
-  #     name                = "nat_rule_collection1_rule1"
-  #     protocols           = ["TCP", "UDP"]
-  #     source_addresses    = ["10.0.0.1", "10.0.0.2"]
-  #     destination_address = "192.168.1.1"
-  #     destination_ports   = ["80"]
-  #     translated_address  = "192.168.0.1"
-  #     translated_port     = "8080"
-  #   }
-  # }
 }
